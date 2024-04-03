41662c71621b7504310c2b044d88fabdb17bad81
jeremytchang <78522362+jeremytchang@users.noreply.github.com>
2024-04-03 20:09:42 +0000

    chore: Fix build (#19)
    This is the error we're getting in build: 
    ```
    ERROR in ./node_modules/vega-label/build/vega-label.module.js 557:33
    Module parse failed: Unexpected token (557:33)
    You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders
    |       return false;
    |     }
    >     let textWidth = d.textWidth ?? 0,
    |       dx,
    |       dy,
     @ ./node_modules/vega/build/vega.module.js 11:0-36 33:48-53
     @ ./node_modules/vega-embed/build/vega-embed.module.js
     @ ./src/histogram.js
     ```
     
    It looks suspiciously like this one: https://github.com/vega/vega/issues/3683
    I added babel-loader babel core and preset-env and updated the webpack config to use babel with the hopefully the right preset and plugin, but it still doesn't fix it.

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,19 @@ var webpackConfig = {
         test: /\.css$/i,
         loader: ["style-loader", "css-loader"],
       },
+      { 
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ],
+            plugins: ['@babel/plugin-transform-nullish-coalescing-operator', '@babel/plugin-transform-optional-chaining']
+          }
+        }
+      },
       {
         test: /\.(woff|woff2|ttf|otf)$/,
         loader: "url-loader",
