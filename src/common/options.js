b900a7c9ec5a25f3482ab690ee0e8b218aeda942
Minhaz Kazi <vlookup@google.com>
2026-06-04 16:50:18 +0000

    Fix OSPO compliance issues

--- src/common/options.js
+++ src/common/options.js
@@ -1,3 +1,8 @@
+// © 2020 Google LLC.  All rights reserved.
+//
+// This software is subject to the Google Cloud Terms of Service, as
+// modified by the "General Software Terms" of the Google Cloud Service Specific Terms, available at: https://cloud.google.com/terms/service-terms.
+
 export const FILTERED_LABELS = "_filterlabels";
 export const baseOptions = {
   // BINNING
--- src/vega_specs/charts/HeatChart.js
+++ src/vega_specs/charts/HeatChart.js
@@ -1,3 +1,8 @@
+// © 2020 Google LLC.  All rights reserved.
+//
+// This software is subject to the Google Cloud Terms of Service, as
+// modified by the "General Software Terms" of the Google Cloud Service Specific Terms, available at: https://cloud.google.com/terms/service-terms.
+
 import Heatmap from "../vega_components/Heatmap";
 import useLayers from "../vega_components/useLayers";
 
